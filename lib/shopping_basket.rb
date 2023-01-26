31b11c82524450940a2996152697dc8504bef23b
Gilberto Villa <ingilniero@gmail.com>
2023-01-26 17:55:00 -0600

    Styling suggestions

--- lib/shopping_basket.rb
+++ lib/shopping_basket.rb
@@ -1,6 +1,7 @@
 require "round"
+
 class ShoppingBasket
-  # attr_accessor :sale_taxes, :total
+  attr_reader :total
 
   def initialize(products)
     @products = products
@@ -12,35 +13,30 @@ class ShoppingBasket
 
   def inputs_str
     str_output = ""
-    @products.each do |p|
-      str_output << "\n#{p.quantity} #{p.name} at: #{p.price}"
+    @products.each do |product|
+      str_output << "\n#{product.quantity} #{product.name} at: #{product.price}"
     end
     str_output
   end
 
   def outputs_str
     str_output = ""
-    @products.each do |p|
+    @products.each do |product|
       str_output << "\n#{p.quantity} #{p.name}: #{p.amount}"
     end
-    str_output << "\nSales Taxes: #{sale_taxes()}"
-    str_output << "\nTotal: #{total()} \n\n"
+    str_output << "\nSales Taxes: #{sale_taxes}"
+    str_output << "\nTotal: #{total} \n\n"
     str_output
   end
 
   def get_sale_taxes
-    @products.each do |p|
-      @total += p.amount
-      @sale_taxes += p.get_taxes
+    @products.each do |product|
+      @total += product.amount
+      @sale_taxes += product.get_taxes
     end
   end
 
-  # getters 
   def sale_taxes
-    Round.nears_05(@sale_taxes)
-  end
-
-  def total
-    @total
+    Round.nearest_05(@sale_taxes)
   end
 end
